28916fd5a4b35954fbbe96e6a3ce86c386fe9529
Code Ass <aycabta@gmail.com>
2016-08-29 00:05:18 +0900

    Add fixture method to test helper

--- test/helper.rb
+++ test/helper.rb
@@ -2,3 +2,19 @@ $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'deviantart'
 
 require 'minitest/autorun'
+require 'json'
+
+def fixture_path
+  File.expand_path('../fixtures', __FILE__)
+end
+
+def fixture(file)
+  data = File.new(fixture_path + '/' + file).read
+  data.instance_eval do
+    def json
+      @json ||= JSON.parse(self)
+    end
+  end
+  data
+end
+
