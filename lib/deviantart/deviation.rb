0cbcac71a82723d69579c8478aba0cccc6a4fc81
Code Ass <aycabta@gmail.com>
2016-08-30 01:58:06 +0900

    Rename variable, id with deviationid

--- lib/deviantart/deviation.rb
+++ lib/deviantart/deviation.rb
@@ -1,7 +1,7 @@
 module DeviantArt
   module Deviation
-    def get_deviation(id)
-      perform(:get, "/api/v1/oauth2/deviation/#{id}")
+    def get_deviation(deviationid)
+      perform(:get, "/api/v1/oauth2/deviation/#{deviationid}")
     end
   end
 end
