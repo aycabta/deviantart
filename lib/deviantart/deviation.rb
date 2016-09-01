168643407c40a58bdd0d61c20be1a61638d45e8c
Code Ass <aycabta@gmail.com>
2016-09-01 15:44:03 +0900

    Implement Deviation#get_deviation_content

--- lib/deviantart/deviation.rb
+++ lib/deviantart/deviation.rb
@@ -3,6 +3,10 @@ module DeviantArt
     def get_deviation(deviationid)
       perform(:get, "/api/v1/oauth2/deviation/#{deviationid}")
     end
+
+    def get_deviation_content(deviationid)
+      perform(:get, "/api/v1/oauth2/deviation/content", { deviationid: deviationid })
+    end
   end
 end
 
