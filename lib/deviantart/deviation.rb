d73b43e07e2997b4cb899fff6f58e502d9c8dd83
Code Ass <aycabta@gmail.com>
2016-09-12 08:41:50 +0900

    Implement /deviation/whofaved

--- lib/deviantart/deviation.rb
+++ lib/deviantart/deviation.rb
@@ -8,6 +8,14 @@ module DeviantArt
       perform(:get, "/api/v1/oauth2/deviation/content", { deviationid: deviationid })
     end
 
+    def get_deviation_whofaved(deviationid, offset: 0, limit: 10)
+      params = {}
+      params['deviationid'] = deviationid
+      params['offset'] = offset if offset != 0
+      params['limit'] = limit if limit != 10
+      perform(:get, "/api/v1/oauth2/deviation/whofaved", params)
+    end
+
     def download_deviation(deviationid)
       perform(:get, "/api/v1/oauth2/deviation/download/#{deviationid}")
     end
