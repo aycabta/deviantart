23a6619ab20052941e80bd11a3dbcc4a7e95f414
Code Ass <aycabta@gmail.com>
2016-09-14 23:16:45 +0900

    Implement /user/friends as get_friends

--- lib/deviantart/user.rb
+++ lib/deviantart/user.rb
@@ -6,6 +6,13 @@ module DeviantArt
       params['ext_galleries'] = ext_galleries if ext_galleries
       perform(:get, "/api/v1/oauth2/user/profile/#{username.nil? ? '' : username}", params)
     end
+
+    def get_friends(username: nil, offset: 0, limit: 10)
+      params = {}
+      params['offset'] = offset if offset != 0
+      params['limit'] = limit if limit != 10
+      perform(:get, "/api/v1/oauth2/user/friends/#{username.nil? ? '' : username}", params)
+    end
   end
 end
 
