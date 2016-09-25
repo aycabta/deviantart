287d140ff1ec25e1e82315c6f03d88c036754031
Code Ass <aycabta@gmail.com>
2016-09-25 10:36:31 +0900

    Add test for #get_privacy

--- test/data_test.rb
+++ test/data_test.rb
@@ -20,4 +20,19 @@ describe DeviantArt::Data do
       assert_includes(result, 'results')
     end
   end
+  describe '#get_privacy' do
+    before do
+      @privacy = fixture('data_privacy.json')
+      stub_da_request(
+        method: :get,
+        url: "https://#{DeviantArt::Client.host}/api/v1/oauth2/data/privacy",
+        da: @da,
+        body: @privacy)
+    end
+    it 'requests the correct resource' do
+      result = @da.get_privacy
+      assert_equal(result.class, Hash)
+      assert_includes(result, 'text')
+    end
+  end
 end
