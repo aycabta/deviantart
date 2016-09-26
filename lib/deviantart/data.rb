3838336112f29470a2563094828ea895155bcc24
Code Ass <aycabta@gmail.com>
2016-09-27 00:17:14 +0900

    Implement /data/submission as #get_submission

--- lib/deviantart/data.rb
+++ lib/deviantart/data.rb
@@ -9,6 +9,11 @@ module DeviantArt
     def get_privacy
       perform(:get, '/api/v1/oauth2/data/privacy')
     end
+
+    # Returns the DeviantArt Submission Policy
+    def get_submission
+      perform(:get, '/api/v1/oauth2/data/submission')
+    end
   end
 end
 
