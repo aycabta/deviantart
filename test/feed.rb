9b145ea7393fd57ec852c6a86ce1fede3fdf6df3
Code Ass <aycabta@gmail.com>
2016-09-29 22:33:07 +0900

    Add test for #get_feed

---
+++ test/feed.rb
@@ -0,0 +1,23 @@
+require 'helper'
+require 'deviantart'
+
+describe DeviantArt::Feed do
+  before(:all) do
+    @da, @credentials = create_da
+  end
+  describe '#get_collections_folders' do
+    before do
+      @feed_home = fixture('feed_home.json')
+      stub_da_request(
+        method: :get,
+        url: %r`^https://#{DeviantArt::Client.host}/api/v1/oauth2/feed/home`,
+        da: @da,
+        body: @feed_home)
+    end
+    it 'requests the correct resource' do
+      result = @da.get_feed
+      assert_equal(result.class, Hash)
+      assert_includes(result, 'items')
+    end
+  end
+end
