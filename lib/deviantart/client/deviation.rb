e8d4355dde51f5d7672dc44b2efb02551e7a568a
Code Ass <aycabta@gmail.com>
2016-10-28 21:26:02 +0900

    Use Deviation models for access API

--- lib/deviantart/client/deviation.rb
+++ lib/deviantart/client/deviation.rb
@@ -1,14 +1,19 @@
+require 'deviantart/deviation'
+require 'deviantart/deviation/content'
+require 'deviantart/deviation/whofaved'
+require 'deviantart/deviation/download'
+
 module DeviantArt
   class Client
     module Deviation
       # Fetch a deviation details
       def get_deviation(deviationid)
-        perform(:get, "/api/v1/oauth2/deviation/#{deviationid}")
+        DeviantArt::Deviation.new(perform(:get, "/api/v1/oauth2/deviation/#{deviationid}"))
       end
 
       # Fetch full data that is not included in the main devaition details
       def get_deviation_content(deviationid)
-        perform(:get, '/api/v1/oauth2/deviation/content', { deviationid: deviationid })
+        DeviantArt::Deviation::Content.new(perform(:get, '/api/v1/oauth2/deviation/content', { deviationid: deviationid }))
       end
 
       # Fetch a list of users who faved the deviation
@@ -17,12 +22,12 @@ module DeviantArt
         params['deviationid'] = deviationid
         params['offset'] = offset if offset != 0
         params['limit'] = limit if limit != 10
-        perform(:get, '/api/v1/oauth2/deviation/whofaved', params)
+        DeviantArt::Deviation::WhoFaved.new(perform(:get, '/api/v1/oauth2/deviation/whofaved', params))
       end
 
       # Get the original file download (if allowed)
       def download_deviation(deviationid)
-        perform(:get, "/api/v1/oauth2/deviation/download/#{deviationid}")
+        DeviantArt::Deviation::Download.new(perform(:get, "/api/v1/oauth2/deviation/download/#{deviationid}"))
       end
 
       # TODO: embeddedcontent, metadata
