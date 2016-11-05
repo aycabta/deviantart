441f1d26429747a5315eabfc54f439731111f405
Code Ass <aycabta@gmail.com>
2016-11-06 00:45:08 +0900

    Add accessor to Status model

--- lib/deviantart/status.rb
+++ lib/deviantart/status.rb
@@ -2,6 +2,7 @@ require 'deviantart/base'
 
 module DeviantArt
   class Status < Base
+    attr_accessor :statusid, :body, :ts, :url, :comments_count, :is_share, :is_deleted, :author, :items
     point_to_class [:author], DeviantArt::User
     point_to_class [:items, :[], :status], DeviantArt::Status
     point_to_class [:items, :[], :deviation], DeviantArt::Deviation
