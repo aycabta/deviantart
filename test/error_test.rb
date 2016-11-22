3a91fd31ff409eb28db1ceb2feb791c0b15ba712
Code Ass <aycabta@gmail.com>
2016-11-23 06:18:06 +0900

    Add error_test.rb

---
+++ test/error_test.rb
@@ -0,0 +1,23 @@
+require 'helper'
+require 'deviantart'
+
+describe DeviantArt::Error do
+  before(:all) do
+    @da = DeviantArt.new
+  end
+  describe '#get_deviation' do
+    before do
+      @error = fixture('deviation.json')
+      @dummyid = 'dummy-id'
+      stub_da_request(
+        method: :get,
+        url: "https://#{@da.host}/api/v1/oauth2/deviation/#{@dummyid}",
+        body: @error, status_code: 404)
+    end
+    it 'requests the correct resource' do
+      result = @da.get_deviation(@dummyid)
+      assert_instance_of(DeviantArt::Error, result)
+      assert_equal(404, result.status_code)
+    end
+  end
+end
