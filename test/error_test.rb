94dae1c08d4bd6f59bae7fdd5db400e11e3b06b1
Code Ass <aycabta@gmail.com>
2016-11-25 00:04:53 +0900

    Add test for 401 error

--- test/error_test.rb
+++ test/error_test.rb
@@ -23,4 +23,22 @@ describe DeviantArt::Error do
       assert_equal("Api endpoint not found.", result.error_description)
     end
   end
+  describe '#get_deviation' do
+    before do
+      @error = fixture('error_401.json')
+      @deviation = fixture('deviation.json')
+      stub_da_request(
+        method: :get,
+        url: "https://#{@da.host}/api/v1/oauth2/deviation/#{@deviation.json['deviationid']}",
+        body: @error, status_code: 401)
+    end
+    it 'requests the correct resource' do
+      result = @da.get_deviation(@deviation.json['deviationid'])
+      assert_instance_of(DeviantArt::Error, result)
+      assert_equal(401, result.status_code)
+      assert_equal("error", result.status)
+      assert_equal("invalid_request", result.error)
+      assert_equal("Must provide an access_token to access this resource.", result.error_description)
+    end
+  end
 end
