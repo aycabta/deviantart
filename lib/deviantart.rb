2c662b48235f4f8d7aa145338502967dacb2e64a
Code Ass <aycabta@gmail.com>
2016-11-30 07:05:15 +0900

    Add comment for backward compatibility to DeviantArt.new

--- lib/deviantart.rb
+++ lib/deviantart.rb
@@ -7,6 +7,7 @@ require 'deviantart/status'
 
 module DeviantArt
   # Bypass args and block to DeviantArt::Client
+  # ...for backward compatibility
   def self.new(*args, &block)
     DeviantArt::Client.new(*args, &block)
   end
