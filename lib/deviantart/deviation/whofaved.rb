d361afaaff0fb0e4f66acd385a43bbdd687b0f8d
Code Ass <aycabta@gmail.com>
2016-12-02 10:22:08 +0900

    Add accessor for Deviation::WhoFaved

--- lib/deviantart/deviation/whofaved.rb
+++ lib/deviantart/deviation/whofaved.rb
@@ -2,6 +2,7 @@ require 'deviantart/base'
 
 module DeviantArt
   class Deviation::WhoFaved < Base
+    attr_accessor :has_more, :next_offset, :results
     point_to_class [:results, :[], :user], DeviantArt::User
   end
 end
