92c9b7aa2a4157230303297829283559fe356431
Code Ass <aycabta@gmail.com>
2016-12-06 01:05:14 +0900

    Implement #get_deviation_metadata

--- lib/deviantart/client/deviation.rb
+++ lib/deviantart/client/deviation.rb
@@ -3,6 +3,7 @@ require 'deviantart/deviation/content'
 require 'deviantart/deviation/whofaved'
 require 'deviantart/deviation/download'
 require 'deviantart/deviation/embeddedcontent'
+require 'deviantart/deviation/metadata'
 
 module DeviantArt
   class Client
@@ -25,6 +26,17 @@ module DeviantArt
         params['offset'] = offset if offset != 0
         params['limit'] = limit if limit != 10
         perform(DeviantArt::Deviation::EmbeddedContent, :get, '/api/v1/oauth2/deviation/embeddedcontent', params)
+      end
+
+      # Fetch deviation metadata for a set of deviations.
+      # This endpoint is limited to 50 deviations per query when fetching the base data and 10 when fetching extended data.
+      def get_deviation_metadata(deviationids, ext_submission: false, ext_camera: false, ext_stats: false, ext_collection: false)
+        params = { deviationids: deviationids.is_a?(Enumerable) ? deviationids : [deviationids] }
+        params['ext_submission'] = ext_submission
+        params['ext_camera'] = ext_camera
+        params['ext_stats'] = ext_stats
+        params['ext_collection'] = ext_collection
+        perform(DeviantArt::Deviation::Metadata, :get, '/api/v1/oauth2/deviation/metadata', params)
       end
 
       # Fetch a list of users who faved the deviation
