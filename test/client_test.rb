3349a98386e25ac5b30e9e5174d066eba4d8226f
Code Ass <aycabta@gmail.com>
2016-12-11 19:19:16 +0900

    Add test for #refresh_access_token with Client Credentials

--- test/client_test.rb
+++ test/client_test.rb
@@ -2,6 +2,7 @@ require 'helper'
 require 'deviantart'
 require 'deviantart/authorization_code'
 require 'deviantart/authorization_code/refresh_token'
+require 'deviantart/client_credentials/access_token'
 
 describe DeviantArt::Client do
   describe '#refresh_access_token with Authorization Code Grant' do
@@ -45,6 +46,33 @@ describe DeviantArt::Client do
         assert_instance_of(String, refresh_token)
       end
       @da.refresh_access_token
+    end
+  end
+  describe '#refresh_access_token with Client Credentials Grant' do
+    before do
+      credentials_input = fixture('credentials-input.json')
+      @da = DeviantArt::Client.new do |config|
+        config.client_id = credentials_input.json['client_credentials']['client_id']
+        config.client_secret = credentials_input.json['client_credentials']['client_secret']
+        config.grant_type = :client_credentials
+        config.access_token_auto_refresh = true
+      end
+      @refresh_client_credentials = fixture('refresh_client_credentials.json')
+      stub_da_request(
+        method: :post,
+        url: "https://#{DeviantArt::Client.default_host}/oauth2/token",
+        status_code: 200,
+        body: @refresh_client_credentials)
+    end
+    it 'requests the correct resource' do
+      @da.access_token = nil
+      assert_nil(@da.access_token)
+      resp = @da.refresh_access_token
+      assert(!@da.access_token.nil?)
+      assert_instance_of(DeviantArt::ClientCredentials::AccessToken, resp)
+      assert_equal('Bearer', resp.token_type)
+      assert_equal(3600, resp.expires_in)
+      assert_equal('success', resp.status)
     end
   end
   describe '#on_refresh_access_token with Client Credentials Grant' do
