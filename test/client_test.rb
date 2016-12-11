a1df8697e49cca9f2089726ddfcb4de31b389048
Code Ass <aycabta@gmail.com>
2016-12-11 18:54:16 +0900

    Add test for #on_refresh_access_token with Client Credentials

--- test/client_test.rb
+++ test/client_test.rb
@@ -47,4 +47,28 @@ describe DeviantArt::Client do
       @da.refresh_access_token
     end
   end
+  describe '#on_refresh_access_token with Client Credentials Grant' do
+    before do
+      credentials_input = fixture('credentials-input.json')
+      @da = DeviantArt::Client.new do |config|
+        config.client_id = credentials_input.json['client_credentials']['client_id']
+        config.client_secret = credentials_input.json['client_credentials']['client_secret']
+        config.grant_type = :client_credentials
+        config.access_token_auto_refresh = true
+      end
+      @refresh_client_credentials = fixture('refresh_client_credentials.json')
+      stub_da_request(
+        method: :post,
+        url: "https://#{DeviantArt::Client.default_host}/oauth2/token",
+        status_code: 200,
+        body: @refresh_client_credentials)
+    end
+    it 'requests the correct resource' do
+      @da.access_token = nil
+      @da.on_refresh_access_token do |access_token|
+        assert_instance_of(String, access_token)
+      end
+      @da.refresh_access_token
+    end
+  end
 end
