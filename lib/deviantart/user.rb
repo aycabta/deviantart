339efaf7244a5adaa84532b768a24e329a7fb34a
Code Ass <aycabta@gmail.com>
2017-01-21 08:40:38 +0900

    Add inspect to User

--- lib/deviantart/user.rb
+++ lib/deviantart/user.rb
@@ -4,5 +4,13 @@ module DeviantArt
   class User < Base
     attr_accessor :userid, :username, :usericon, :type, :is_watching, :details, :geo, :profile, :stats
     point_to_class [:profile, :profile_pic], DeviantArt::Deviation
+
+    def inspect
+      "#{self.class.name}: #{@username} #{@userid}"
+    end
+
+    def to_s
+      inspect
+    end
   end
 end
