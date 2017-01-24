ca312489e8ff7305de71026b48b2fecb25724682
Code Ass <aycabta@gmail.com>
2017-01-24 23:45:44 +0900

    Fix description for Base test

--- test/base_test.rb
+++ test/base_test.rb
@@ -3,11 +3,11 @@ require 'deviantart'
 require 'deviantart/base'
 
 describe DeviantArt::Base do
-  describe 'DeviantArt::Base' do
+  describe '#inspect' do
     before do
       @base = DeviantArt::Base.new({})
     end
-    it 'has inspect' do
+    it 'returns name' do
       assert_equal('DeviantArt::Base', @base.inspect)
       assert_equal(@base.to_s, @base.inspect)
     end
