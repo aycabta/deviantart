a5f781e1854d2e52a8c2f0ea0b2c2bbf27bba67c
Code Ass <aycabta@gmail.com>
2017-01-25 00:25:23 +0900

    Add test for Base.point_to_class

--- test/base_test.rb
+++ test/base_test.rb
@@ -1,6 +1,7 @@
 require 'helper'
 require 'deviantart'
 require 'deviantart/base'
+require 'deviantart/deviation'
 
 describe DeviantArt::Base do
   describe '#inspect' do
@@ -10,6 +11,27 @@ describe DeviantArt::Base do
     it 'returns name' do
       assert_equal('DeviantArt::Base', @base.inspect)
       assert_equal(@base.to_s, @base.inspect)
+    end
+  end
+  describe 'DeviantArt::Base' do
+    before do
+      klass = Class.new(DeviantArt::Base)
+      klass.__send__(:attr_accessor, :has_more, :next_offset, :name, :results)
+      klass.point_to_class [:results, :[]], DeviantArt::Deviation
+      @instance = klass.new({
+        has_more: false,
+        next_offset: nil,
+        name: 'test',
+        results: [
+          { title: 'aaa' },
+          { title: 'bbb' }
+        ]
+      })
+    end
+    it '' do
+      assert_kind_of(DeviantArt::Base, @instance)
+      assert_instance_of(Array, @instance.results)
+      assert_instance_of(DeviantArt::Deviation, @instance.results.first)
     end
   end
 end
