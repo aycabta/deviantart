91d04d3d9d520f4543bdb8588d2ba7628e5ec0c4
Code Ass <aycabta@gmail.com>
2017-02-26 23:01:12 +0900

    Add #profile

--- lib/deviantart/client/feed.rb
+++ lib/deviantart/client/feed.rb
@@ -1,5 +1,6 @@
 require 'deviantart/feed'
 require 'deviantart/feed/home'
+require 'deviantart/feed/profile'
 
 module DeviantArt
   class Client
@@ -10,6 +11,13 @@ module DeviantArt
         params['cursor'] = cursor unless cursor.nil?
         params['mature_content'] = mature_content
         perform(DeviantArt::Feed::Home, :get, '/api/v1/oauth2/feed/home', params)
+      end
+
+      # Fetch Profile Feed
+      def profile(cursor: nil)
+        params = {}
+        params['cursor'] = cursor unless cursor.nil?
+        perform(DeviantArt::Feed::Profile, :get, '/api/v1/oauth2/feed/profile', params)
       end
 
       # TODO: home/{bucketid}, notifications, profile, settings, settings/update
