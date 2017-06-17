890f065e5aec76ebd15b1356d9c7a42650912abe
Code Ass <aycabta@gmail.com>
2017-06-18 01:33:58 +0900

    Add artist_specialty.rb

---
+++ lib/deviantart/artist_specialty.rb
@@ -0,0 +1,13 @@
+module DeviantArt
+  module ArtistSpecialty
+    ArtisanCrafts = 1
+    DesignAndInterfaces = 2
+    DigitalArt = 3
+    FilmAndAnimation = 4
+    Literature = 5
+    Photography = 6
+    TraditionalArt = 7
+    Other = 8
+    Varied = 9
+  end
+end
