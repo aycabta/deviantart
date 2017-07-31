0013f0917f125500a482ea61d2b6bc6212234d00
Code Ass <aycabta@gmail.com>
2017-07-31 11:15:06 +0900

    Add DeviantArt::Error#inspect

--- lib/deviantart/error.rb
+++ lib/deviantart/error.rb
@@ -8,5 +8,16 @@ module DeviantArt
       super(json)
       @status_code = status_code
     end
+
+    def inspect
+      messages = []
+      messages << self.class.name
+      messages << "status_code: #{@status_code}"
+      messages << "error_code: #{@error_code}" if @error_code
+      messages << "error: #{@error}" if @error
+      messages << "error_description: #{@error_description}" if @error_description
+      messages << "error_details: #{@error_details}" if @error_details
+      messages.join("\n")
+    end
   end
 end
