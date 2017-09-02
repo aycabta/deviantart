d9351240405a41aec52ec6f685093e8de976c851
Code Ass <aycabta@gmail.com>
2017-09-03 03:24:59 +0900

    Fix ghost-method doc

--- lib/deviantart/user.rb
+++ lib/deviantart/user.rb
@@ -5,31 +5,31 @@ module DeviantArt
     ##
     # :method: userid
     # The user's ID as UUID, for example 80D3D72E-1B28-4F15-E529-A2EADB60B093.
-    #
+
     # :method: username
     # The user's name, for example aycabta.
-    #
+
     # :method: usericon
     # The user's icon URL.
-    #
+
     # :method: type
     # It'll get "regular" or ...?
-    #
+
     # :method: is_watching
     # Is authorized user watching the user?
-    #
+
     # :method: details
     # Some values; age, joindate and sex.
-    #
+
     # :method: geo
     # Some values; country, countryid and timezone.
-    #
+
     # :method: profile
     # Some values; artist_level, artist_speciality, cover_photo, real_name, tagline, user_is_artist, profile_pic and website.
-    #
+
     # :method: stats
     # Some values; friends and watchers.
-    #
+
     attr_accessor :userid, :username, :usericon, :type, :is_watching, :details, :geo, :profile, :stats
     point_to_class [:profile, :profile_pic], DeviantArt::Deviation
 
