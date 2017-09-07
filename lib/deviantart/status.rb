5d756b2e77bdf5982b674f9218b7a11965bdaf35
Code Ass <aycabta@gmail.com>
2017-09-08 00:58:10 +0900

    Add ghost-methods to Status

--- lib/deviantart/status.rb
+++ lib/deviantart/status.rb
@@ -2,6 +2,33 @@ require 'deviantart/base'
 
 module DeviantArt
   class Status < Base
+    # :method: statusid
+    #
+
+    # :method: body
+    #
+
+    # :method: ts
+    #
+
+    # :method: url
+    #
+
+    # :method: comments_count
+    #
+
+    # :method: is_share
+    #
+
+    # :method: is_deleted
+    #
+
+    # :method: author
+    #
+
+    # :method: items
+    #
+
     attr_accessor :statusid, :body, :ts, :url, :comments_count, :is_share, :is_deleted, :author, :items
     point_to_class [:author], DeviantArt::User
     point_to_class [:items, :[], :status], DeviantArt::Status
