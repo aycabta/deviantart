46de5892b282a2a014fee842f41137eb5407afe9
Code Ass <aycabta@gmail.com>
2017-09-07 09:10:12 +0900

    Add ghost-methods to Deviation

--- lib/deviantart/deviation.rb
+++ lib/deviantart/deviation.rb
@@ -2,6 +2,81 @@ require 'deviantart/base'
 
 module DeviantArt
   class Deviation < Base
+    # :method: deviationid
+    #
+
+    # :method: printid
+    #
+
+    # :method: url
+    #
+
+    # :method: title
+    #
+
+    # :method: category
+    #
+
+    # :method: category_path
+    #
+
+    # :method: is_favourited
+    #
+
+    # :method: is_deleted
+    #
+
+    # :method: author
+    #
+
+    # :method: stats
+    #
+
+    # :method: published_time
+    #
+
+    # :method: allows_comments
+    #
+
+    # :method: preview
+    #
+
+    # :method: content
+    #
+
+    # :method: thumbs
+    #
+
+    # :method: videos
+    #
+
+    # :method: flash
+    #
+
+    # :method: daily_deviation
+    #
+
+    # :method: excerpt
+    #
+
+    # :method: is_mature
+    #
+
+    # :method: is_downloadable
+    #
+
+    # :method: download_filesize
+    #
+
+    # :method: challenge
+    #
+
+    # :method: challenge_entry
+    #
+
+    # :method: motion_book
+    #
+
     attr_accessor :deviationid, :printid, :url, :title, :category, :category_path, :is_favourited, :is_deleted, :author, :stats
     attr_accessor :published_time, :allows_comments, :preview, :content, :thumbs, :videos, :flash, :daily_deviation
     attr_accessor :excerpt, :is_mature, :is_downloadable, :download_filesize, :challenge, :challenge_entry, :motion_book
