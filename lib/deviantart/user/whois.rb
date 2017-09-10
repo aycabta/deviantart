244f1bd7ce3ddd73e0218ca5af9f58ebc9a4ac17
Code Ass <aycabta@gmail.com>
2017-09-10 15:08:20 +0900

    Add ghost-methods to User::Whois

--- lib/deviantart/user/whois.rb
+++ lib/deviantart/user/whois.rb
@@ -2,6 +2,9 @@ require 'deviantart/base'
 
 module DeviantArt
   class User::Whois < Base
+    # :method: results
+    #
+
     attr_accessor :results
     point_to_class [:results, :[]], DeviantArt::User
   end
