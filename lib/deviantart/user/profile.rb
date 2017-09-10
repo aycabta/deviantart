5f7311058f8db84095738df3b01d34c1a73fa2e3
Code Ass <aycabta@gmail.com>
2017-09-10 15:07:54 +0900

    Add ghost-methods to User::Profile

--- lib/deviantart/user/profile.rb
+++ lib/deviantart/user/profile.rb
@@ -2,6 +2,60 @@ require 'deviantart/base'
 
 module DeviantArt
   class User::Profile < Base
+    # :method: user
+    #
+
+    # :method: is_watching
+    #
+
+    # :method: profile_url
+    #
+
+    # :method: user_is_artist
+    #
+
+    # :method: artist_level
+    #
+
+    # :method: artist_specialty
+    #
+
+    # :method: real_name
+    #
+
+    # :method: tagline
+    #
+
+    # :method: countryid
+    #
+
+    # :method: country
+    #
+
+    # :method: website
+    #
+
+    # :method: bio
+    #
+
+    # :method: cover_photo
+    #
+
+    # :method: profile_pic
+    #
+
+    # :method: last_status
+    #
+
+    # :method: stats
+    #
+
+    # :method: collections
+    #
+
+    # :method: galleries
+    #
+
     attr_accessor :user, :is_watching, :profile_url, :user_is_artist, :artist_level, :artist_specialty, :real_name, :tagline
     attr_accessor :countryid, :country, :website, :bio, :cover_photo, :profile_pic, :last_status, :stats, :collections, :galleries
     point_to_class [:user], DeviantArt::User
