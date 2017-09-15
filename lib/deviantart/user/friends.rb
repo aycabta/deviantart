813c408c6f54ffac83015e97bf7d1ba9e4566f54
Code Ass <aycabta@gmail.com>
2017-09-16 08:12:05 +0900

    Add ghost-methods to User::Friends

--- lib/deviantart/user/friends.rb
+++ lib/deviantart/user/friends.rb
@@ -2,6 +2,15 @@ require 'deviantart/base'
 
 module DeviantArt
   class User::Friends < Base
+    # :method: has_more
+    #
+
+    # :method: next_offset
+    #
+
+    # :method: results
+    #
+
     attr_accessor :has_more, :next_offset, :results
     point_to_class [:results, :[], :user], DeviantArt::User
   end
