fb07b3fef071ce6a31f8a84342526da10ed47c41
Code Ass <aycabta@gmail.com>
2017-09-17 16:01:31 +0900

    Add ghost-methods to User::Statuses

--- lib/deviantart/user/statuses.rb
+++ lib/deviantart/user/statuses.rb
@@ -2,6 +2,15 @@ require 'deviantart/base'
 
 module DeviantArt
   class User::Statuses < Base
+    # :method: has_more
+    #
+
+    # :method: next_offset
+    #
+
+    # :method: results
+    #
+
     attr_accessor :has_more, :next_offset, :results
     point_to_class [:results, :[]], DeviantArt::Status
   end
