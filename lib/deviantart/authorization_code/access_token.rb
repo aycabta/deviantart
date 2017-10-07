02b92f2d5bf417aa86f7b72cbe9bb38f517bb387
Code Ahss <aycabta@gmail.com>
2017-10-07 21:22:18 +0900

    Add document for AuthorizationCode::AcessToken

--- lib/deviantart/authorization_code/access_token.rb
+++ lib/deviantart/authorization_code/access_token.rb
@@ -1,6 +1,8 @@
 require 'deviantart/base'
 
 module DeviantArt
+  ##
+  # Access token class for authorization code grant
   class AuthorizationCode::AccessToken < Base
     attr_accessor :expires_in, :status, :access_token, :token_type, :refresh_token, :scope
   end
