fe7dfe996e8fbdb2349f5751fe4a5e0ba7727e99
Code Ass <aycabta@gmail.com>
2017-10-18 15:49:10 +0900

    Write description of Gallery::All

--- lib/deviantart/gallery/all.rb
+++ lib/deviantart/gallery/all.rb
@@ -3,13 +3,14 @@ require 'deviantart/base'
 module DeviantArt
   class Gallery::All < Base
     # :method: has_more
-    #
+    # Boolean field, indicating whether or not there is more data available.
 
     # :method: next_offset
-    #
+    # To fetch the next page of data, send the value of this field as an
+    # offset or cursor to the endpoint.
 
     # :method: results
-    #
+    # Friends list as DeviantArt::Deviation.
 
     attr_accessor :has_more, :next_offset, :results
     point_to_class [:results, :[]], DeviantArt::Deviation
