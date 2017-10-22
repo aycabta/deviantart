80a2bff57db7afdf0c6f8ffbf947214b50c35089
Code Ass <aycabta@gmail.com>
2017-10-22 22:27:32 +0900

    Write description of User::Watchers

--- lib/deviantart/user/watchers.rb
+++ lib/deviantart/user/watchers.rb
@@ -3,13 +3,14 @@ require 'deviantart/base'
 module DeviantArt
   class User::Watchers < Base
     # :method: has_more
-    #
+    # Boolean field, indicating whether or not there is more data available.
 
     # :method: next_offset
-    #
+    # To fetch the next page of data, send the value of this field as an
+    # offset or cursor to the endpoint.
 
     # :method: results
-    #
+    # Watchers list as DeviantArt::User.
 
     attr_accessor :has_more, :next_offset, :results
     point_to_class [:results, :[], :user], DeviantArt::User
