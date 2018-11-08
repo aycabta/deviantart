f1c065b67c4fb9a32dc0b4b1d75119dbb576f1fa
aycabta <aycabta@gmail.com>
2018-11-09 01:06:06 +0900

    Add doc for Gallery

--- lib/deviantart/gallery.rb
+++ lib/deviantart/gallery.rb
@@ -1,6 +1,8 @@
 require 'deviantart/base'
 
 module DeviantArt
+  ##
+  # Gallery class for API response
   class Gallery < Base
     attr_accessor :has_more, :next_offset, :name, :results
     point_to_class [:results, :[]], DeviantArt::Deviation
